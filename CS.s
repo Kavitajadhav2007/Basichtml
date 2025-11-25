a799ceb698ace3ec0bdaa6d50a2d06fe13e24468
Kavitajadhav2007 <kj216934@gmail.com>
2025-11-25 20:09:38 +0530

    This is revision of HTML

---
+++ CS.s
@@ -0,0 +1,49 @@
+/* element */
+*
+{
+    text-align: center;
+}
+
+/* universal : u b p h1 h2 h3 */
+
+body
+{
+    background-color: black ;
+}
+
+/* element : p (all the uses of p only )  h3 ()all the uses of h3 only */
+
+h3
+{
+    color : green
+}
+
+#RedColor
+{
+    color: Red;
+}
+
+/* id selector : single element unique id . #  */
+
+.BlueColor
+{
+    color : blue;
+}
+
+/* class  : group of elements . .classname  */
+
+b,i{
+    color : white
+}
+
+div p b{
+    color : gold
+}
+
+/* affect group of element */
+
+
+/* types of css  */
+/* inline */
+
+/* types of selectors */
